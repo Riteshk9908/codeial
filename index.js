a07d3d33799389d5205003635abcc86deccaf7ac
Ritesh <riteshkumarx295@gmail.com>
2021-04-11 22:20:23 +0530

    initial commit

--- index.js
+++ index.js
@@ -12,6 +12,3 @@ app.listen(port,function(err){
 });
 
 //there is node_module is not showing ya i also tried same thing happen ? then what is your doubt 
-//github me node_modules add hui joki ni honi
-//chiye thi ? apne phle uko git_ignore me add kiya fir v ?maine same according to lecture replicate kiya bt mere me ni hua
-// dekho jn 
