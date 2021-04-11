5e256f9f847f628cfe5b5be617e247d0c11988d3
Ritesh <riteshkumarx295@gmail.com>
2021-04-11 20:38:12 +0530

    first commit

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ app.listen(port,function(err){
     }
     console.log(`Server is running on port:${port}`);
 });
+
+//there is node_module is not showing ya i also tried same thing happen ? then what is your doubt 
+//github me node_modules add hui joki ni honi
+//chiye thi ? apne phle uko git_ignore me add kiya fir v ?maine same according to lecture replicate kiya bt mere me ni hua
+// dekho jn 
