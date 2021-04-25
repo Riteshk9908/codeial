d3fae68ed8e88ba142809b0dd1deda80a41e05b4
Ritesh <riteshkumarx295@gmail.com>
2021-04-25 12:29:16 +0530

    sign-in done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,12 +1,28 @@
-const User = require('../models/user');
+ const User = require('../models/user');
 
 
 module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title: 'User Profile'
-    })
-}
+    if (req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err, user){
+            if (user){
+                return res.render('user_profile', {
+                    title: "User Profile",
+                    user: user
+                })
+            }else{
+                return res.redirect('/users/sign-in');
 
+            }
+        });
+    }else{
+        return res.redirect('/users/sign-in');
+
+    }
+}
+// module.exports.logout=function(req,res){
+     
+//   return res.redirect('/users/sign-in');
+// }
 
 // render the sign up page
 module.exports.signUp = function(req, res){
@@ -48,5 +64,35 @@ module.exports.create = function(req, res){
 
 // sign in and create a session for the user
 module.exports.createSession = function(req, res){
-    // TODO later
+
+    // steps to authenticate
+    // find the user
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing in'); return}
+        // handle user found
+        if (user){
+
+            // handle password which doesn't match
+            if (user.password != req.body.password){
+                return res.redirect('back');
+            }
+
+            // handle session creation
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+
+        }else{
+            // handle user not found
+
+            return res.redirect('back');
+        }
+
+
+    });
+
+ 
+
+    
+
+    
 }
--- routes/users.js
+++ routes/users.js
@@ -7,10 +7,10 @@ router.get('/profile',usersController.profile);
 
 router.get('/sign-up',usersController.signUp);
 router.get('/sign-in',usersController.signIn);
-
+router.get('/logout',usersController.logout);
 
 router.post('/create',usersController.create);
-
+router.post('/create-session',usersController.createSession);
 
 
 module.exports=router;
